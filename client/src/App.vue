d7141dfb10093d5d9cfb445fbabe9aa8311d295a
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-03-04 18:55:32 -0700

    uninstalled jade

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,17 +7,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      
+    }
+  }
 };
 </script>

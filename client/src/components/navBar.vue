9ecf4f7e37ab17a9e5f6026113f3ebbb7a9e5485
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-03-08 12:15:51 -0600

    updated top bar with image

--- client/src/components/navBar.vue
+++ client/src/components/navBar.vue
@@ -6,17 +6,51 @@
       <v-btn text color="white" @click="snackbar = false">Close</v-btn>
     </v-snackbar>
 
-    <v-app-bar flat app class="grey lighten-4">
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer" text color="primary"></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase">
-        <span class="font-weight-light primary--text">Recipe</span>
-        <span class='primary--text'>App</span>
+    <v-app-bar 
+      flat 
+      app 
+      class="grey lighten-4"
+      shrink-on-scroll
+      prominent
+      src="../assets/med-food-hero.jpg"
+    >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to bottom right, rgba(193, 66, 66, 0.9), rgba(25,32,72,0.8)"
+      ></v-img>
+    </template>
+    <v-app-bar-nav-icon 
+      @click.stop="drawer = !drawer" 
+      text color="black"
+      x-large 
+      dark
+    />
+      <v-toolbar-title class="text-uppercase bold font-weight-black display-1">
+        <span class="font-weight-light black--text">Recipe</span>
+        <span class='black--text'>Tracker</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text color="primary" @click='logout'>
-        <span>Sign Out</span>
-        <v-icon right>mdi-exit-to-app</v-icon>
+      <v-spacer></v-spacer>
+
+      <v-btn icon color="primary">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
+
+      <v-btn icon color="primary">
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon color="primary">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+      <!-- <v-sheet
+        id="scrolling-techniques-5"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-container style="height: 1500px;"></v-container>
+      </v-sheet> -->
     </v-app-bar>
 
     <v-navigation-drawer app v-model='drawer' absolute temporary>
@@ -43,11 +77,21 @@
                 <v-list-item-title v-text="item.text" ></v-list-item-title>
               </v-list-item-content>
             </v-list-item>
+            <v-list-item text mandatory color="primary" @click='logout'>
+              <v-list-item-icon>
+                <v-icon>mdi-exit-to-app</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Sign Out</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
           </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
+
+
 
 <script>
 import addRecipeModal from './addRecipeModal'
@@ -62,6 +106,7 @@ export default {
       drawerItems: [
         { text: 'Dashboard', icon: 'mdi-view-dashboard', route: '/' },
         { text: 'My Profile', icon: 'mdi-account', route: '/profile' },
+        // { text: 'Sign Out', icon: 'mdi-exit-to-app', route:'@logout'}
       ],
       snackbar: false,
     }

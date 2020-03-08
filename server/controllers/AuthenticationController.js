e28eba14bf48d120dd17ba5c3522964cbaa4067b
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-03-07 18:19:54 -0700

    added salting and hashing to passwords and allowed for a user to login

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -1,15 +1,56 @@
 const {Users} = require('../models')
+const crypto = require('crypto')
+
+//helper functions to hash and check hash
+function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.pbkdf2Sync(password, salt, 2048, 32, 'sha512').toString('hex');
+  return [salt, hash].join('$');
+  }
+
+function verifyHash(password, original) {
+  const originalHash = original.split('$')[1];
+  const salt = original.split('$')[0];
+  const hash = crypto.pbkdf2Sync(password, salt, 2048, 32, 'sha512').toString('hex');
+  
+  return hash === originalHash
+  
+  }
 
 module.exports = {
   async register (req,res) {
     try {
-      console.log(req.body)
-      const user = await Users.create(req.body)
-      res.send(user.toJSON())
-    } catch (err) {
-      res.status(400).send({
-        error: 'This email account is already in use'
+      const user = await Users.create({
+        email: req.body.email,
+        password: await hashPassword(req.body.password)
       })
+      res.send(user.toJSON())
+      } catch (err) {
+        res.status(400).send({
+          error: 'This email account is already in use'
+        })
+      }
+    },
+    async login (req,res) {
+      try {
+        const user = await Users.findOne({
+          where: { email: req.body.email }
+        })
+        if (verifyHash(req.body.password, user.password) === true) {
+          res.status(200).send({
+            success: req.body.email
+          })
+        } else {
+          res.status(400).send({
+            error: 'Passwords do not match!'
+          })
+        }
+      } catch (err) {
+        res.status(400).send({
+          error: 'User not found'
+        })
+      }
     }
   }
-}
+
+

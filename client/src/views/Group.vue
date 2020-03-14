94e15943bda7d00eb0cfa4efd075bf8d84a64386
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-03-14 12:01:43 -0600

    updated the find friends section and added keyup event

--- client/src/views/Group.vue
+++ client/src/views/Group.vue
@@ -3,54 +3,52 @@
     <v-content>
       <div class="Group">
         <v-card
-    color="primary"
-    dark
-  >
-    <v-card-title class="primary">
-      Find Friends
-    </v-card-title>
-    <v-card-text>
-    <v-row align='center' wrap>
-      <v-col lg='11' md='10' sm="9">
-        <v-responsive min-width="400">
-          <v-text-field
-            prepend-icon="mdi-account-search"
-            v-model="submitEmail"
-            label="Search friends by email"
-          >
-          </v-text-field>
-        </v-responsive>
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col lg='1' md='2' sm='3'>
-        <v-btn color="white">
-          <v-icon class="black--text" @click="search">mdi-magnify</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-expand-transition>
-      <v-list v-if="foundUser !== '' || foundUser !== 'not found'" class="secondary">
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>User Found!</v-list-item-title>
-            <span v-text='foundUser'></span>
-          </v-list-item-content>
-          <v-btn @click='addUserToGroup'>
-            Add User
-          </v-btn>
-        </v-list-item>
-      </v-list>
-      <v-list v-if="foundUser === 'not found'" class="secondary">
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>User Not Found!</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-expand-transition>
-  </v-card>
+          color="white"
+          elevation='0'
+        >
+          <v-card-title class="white">
+            Find Friends
+          </v-card-title>
+          <v-card-text>
+            <v-row align='center' wrap>
+              <v-col>
+                <v-responsive width="400" min-width="400">
+                  <v-text-field
+                    prepend-icon="mdi-account-search"
+                    v-model="submitEmail"
+                    label="Search friends by email"
+                    @keyup.enter="search"
+                  >
+                  </v-text-field>
+                </v-responsive>
+              </v-col>
+              <v-spacer></v-spacer>
+              <v-col lg='1' md='2' sm='3'>
+                <v-btn class="primary">
+                  <v-icon class="white--text" @click="search" >mdi-magnify</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row >
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-expand-transition>
+            <v-card-text>
+              <v-row wrap align='center' v-if="foundUser !== null">
+                <v-col class="white pa-2" >
+                  <v-responsive width="400" min-width="400">
+                    <span v-text='foundUser'></span>
+                  </v-responsive>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col lg='1' md='2' sm='3'>
+                  <v-btn v-if='foundUser !== "Not Found"' @click='addUserToGroup' class="primary pa-2">
+                    Add User
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-expand-transition>
+        </v-card>
       </div>
     </v-content>
   </v-app>
@@ -62,7 +60,7 @@ import axios from 'axios'
   export default {
     data: () => ({
       descriptionLimit: 60,
-      foundUser: '',
+      foundUser: null,
       isLoading: false,
       submitEmail: null,
     }),
@@ -76,12 +74,17 @@ import axios from 'axios'
             this.foundUser = resp.data.response.email
             this.submitEmail = null
           } else {
-            this.foundUser = 'not found'
+            this.submitEmail = null
+            this.foundUser = 'Not Found'
           }
         }).catch(err => {
           console.log(err)
-            this.foundUser = 'not found'
+            this.submitEmail = null
+            this.foundUser = 'Not Found'
         }).finally(() => (this.isLoading = false))
+      },
+      addUserToGroup() {
+        console.log("up")
       }
     }
   }
